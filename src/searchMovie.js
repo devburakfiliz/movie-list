12e374ad35632c2306fdd6e0fb79dd5e65ca7679
devburakfiliz <dev.burakfiliz@gmail.com>
2022-08-04 12:45:06 +0300

    movie-list

---
+++ src/searchMovie.js
@@ -0,0 +1,19 @@
+import React from 'react';
+export default function  SearchMovie(){
+
+    return(
+        <div>
+            <div className='searchBar'>
+                <div className='mainSearchBar'>
+                    <h2>Search movie</h2>
+                    <input type="text" placeholder='search...'/>
+                </div>
+
+
+
+
+            </div>
+        </div>
+    )
+
+}
